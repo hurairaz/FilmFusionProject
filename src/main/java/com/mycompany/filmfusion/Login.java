554479e88652ee0cb940a76075701d5ef3366e07
hp <hp@192.168.56.1>
2023-12-10 19:20:58 +0500

    CUSTOMER SIDE DONE  ADDS INTO SAVEDLIST AND WISHLIST , CHNAGES PASSWORDS , DISPLAYS ALL MOVIES , PLAY ALL MOVIES , AND dlt FROM SAVEDLIST WISHLIST ALSO POSSIBLE . EACH USER HAS HIS OWN WISHLIST AND SAVED LIST

--- src/main/java/com/mycompany/filmfusion/Login.java
+++ src/main/java/com/mycompany/filmfusion/Login.java
@@ -6,7 +6,8 @@ package com.mycompany.filmfusion;
 import javax.swing.JOptionPane;
 import mongodb.main;
 import mongodb.main.Admin;
-import mongodb.main.Userdata;
+import mongodb.main.User;
+
 
 /**
  *
@@ -17,8 +18,12 @@ public class Login extends javax.swing.JFrame {
     /**
      * Creates new form Login
      */
+     private final main mainInstance;
     public Login() {
         initComponents();
+        mainInstance = main.getInstance();
+        mainInstance.start();
+        
     }
 
     /**
@@ -235,10 +240,11 @@ public class Login extends javax.swing.JFrame {
       
     else
       {
-          Userdata userdata = main.getInstance().getuserfromdb(new Userdata(username, password));
+          User userdata = main.getInstance().getuserfromdb(new User(username, password));
           if( userdata != null )
       {
-          
+        mainInstance.deleteSingleValueFromRecentCollection();
+        mainInstance.addSingleValueToRecentCollection(username);
        Dashboard DashboardFrame =  new Dashboard();
       DashboardFrame.setVisible(true);
       DashboardFrame.pack();
@@ -252,15 +258,48 @@ public class Login extends javax.swing.JFrame {
       }
       
       
-      
+        
   
       
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    
+    
+
     /**
      * @param args the command line arguments
      */
-    
+     public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Settings.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Settings.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Settings.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Settings.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Settings().setVisible(true);
+            }
+        });
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Left;
     private javax.swing.JPanel Right;
